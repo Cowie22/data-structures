e8b49e79f4c77b7b079e7d661a61d08a72cd5899
Cowie22 <rjcowie@ucla.edu>
2019-01-05 16:54:29 -0800

    nothing implemented - scratch code

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,12 +1,40 @@
 var LinkedList = function() {
-  var list = {};
+  var list = {}; 
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value); 
+    if (!list.tail) {
+      list.tail.next = newNode;
+      list.head.next = newNode;    
+    }
+    list.tail.next = newNode;
+     
+    // if (list.head === null) {
+    //   list.head = list.tail;
+    // } 
+      
+      
+    
+    // }
+    
   };
 
   list.removeHead = function() {
+    var result = list.head.value; 
+    if (list.head) {
+      list.head = list.head.next;  
+    }
+  
+    
+    return result;
+    // if (Object.keys(list).length > 0) {
+    //   var result = list.head; 
+    //   delete list.head; 
+    //   //something 
+    //   return result; 
+    // } 
   };
 
   list.contains = function(target) {

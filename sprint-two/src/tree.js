2e7c8790255d3764682ecd328dbecdc2878090ea
Cowie22 <rjcowie@ucla.edu>
2019-01-07 15:04:09 -0800

    working tests implemented for tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -15,16 +15,18 @@ treeMethods.addChild = function(value) {
   this.children.push(newNode); 
 };
 
-treeMethods.contains = function(target) {
-  if (this.value === target) {
+treeMethods.contains = function(target, parent) {
+  parent = parent || this;
+  if (parent.value === target) {
     return true; 
   }
-  if (this.children) {
-    for (let i = 0; i < this.children.length; i++) {
-      var child = this.children[i]; 
-      child.contains(target); 
-    }
+  for (let i = 0; i < parent.children.length; i++) {
+    var child = this.children[i]; 
+    if (child.contains(target, parent.children[i])) {
+      return true;
+    } 
   }
+    return false;
   //check if parent.value === target
     //if not check if parent has children 
   //check each child for value 

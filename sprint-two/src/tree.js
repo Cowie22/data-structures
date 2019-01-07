36875783a79b809e91ed688e17a5b6e514346c35
Cowie22 <rjcowie@ucla.edu>
2019-01-07 11:25:35 -0800

    addchild implemented, specs passing

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,8 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods)
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,9 +11,27 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newNode = Tree(value);
+  this.children.push(newNode); 
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true; 
+  }
+  if (this.children) {
+    for (let i = 0; i < this.children.length; i++) {
+      var child = this.children[i]; 
+      child.contains(target); 
+    }
+  }
+  //check if parent.value === target
+    //if not check if parent has children 
+  //check each child for value 
+    //check if each child has children 
+      //check each for value 
+  
+  
 };
 
 

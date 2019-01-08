c794083b8e76697f5043bece4d4bb8739e69799a
Cowie22 <rjcowie@ucla.edu>
2019-01-07 21:55:40 -0800

    implemented hashtable, all specs passing

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,17 +5,40 @@ var HashTable = function() {
   this._storage = LimitedArray(this._limit);
 };
 
+
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let bucket = this._storage.get(index) || [];
+  let tuple = [k,v];
+  for (let i = 0; i < bucket.length; i++) {
+    let bucketTuple = bucket[i];
+    if (bucketTuple[0] === k) {
+      bucketTuple[1] = v; 
+    }
+  }
+  if (!bucket.includes(tuple)) {
+    bucket.push(tuple);
+  } 
+  this._storage.set(index, bucket);
+  
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let bucket = this._storage.get(index);
+  for (let i = 0; i < bucket.length; i++) {
+    let tuple = bucket[i];
+    if (tuple[0] === k) {
+      return tuple[1]; 
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-};
+  let tuple = this._storage.get(index); 
+  delete tuple[0][1]; 
+};     
 
 
 
